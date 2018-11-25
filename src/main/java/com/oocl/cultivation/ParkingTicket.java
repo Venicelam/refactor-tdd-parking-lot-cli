24a5d68e48ec96b35fe59a2bb1eaa0531820d500
lamve2 <venice.lam@oocl.com>
2018-11-25 15:33:11 +0800

    implement parking ticket

--- src/main/java/com/oocl/cultivation/ParkingTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -1,4 +1,37 @@
 package com.oocl.cultivation;
 
 public class ParkingTicket {
+    private ParkingLot parkingLot;
+    private int lotNumber;
+
+    public ParkingTicket(ParkingLot parkingLot, int lotNumber) {
+        this.parkingLot = parkingLot;
+        this.lotNumber = lotNumber;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public int getLotNumber() {
+        return lotNumber;
+    }
+
+    public void setLotNumber(int lotNumber) {
+        this.lotNumber = lotNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "LotAddress{" +
+                "parkingLot=" + parkingLot +
+                ", lotNumber=" + lotNumber +
+                '}';
+    }
+
 }
+
