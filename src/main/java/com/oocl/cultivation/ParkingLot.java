2fb712b5b56368ba3ac9f88d36c1d399ee8c11b6
lamve2 <venice.lam@oocl.com>
2018-11-25 16:02:56 +0800

    implement parkinglot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,21 +1,81 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Map;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ParkingLot {
-    private final int capacity;
-    private Map<ParkingTicket, Car> cars = new HashMap<>();
 
-    public ParkingLot() {
-        this(10);
+    private int total;
+    private List<Car> lots;
+
+    public int getTotal() {
+        return total;
     }
 
-    public ParkingLot(int capacity) {
-        this.capacity = capacity;
+    public void setTotal(int total) {
+        this.total = total;
     }
 
-    public int getAvailableParkingPosition() {
-        return cars.size() - capacity;
+    public List<Car> getLots() {
+        return lots;
+    }
+
+    public void setLots(List<Car> lots) {
+        this.lots = lots;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingLot{" +
+                "total=" + total +
+                ", lots=" + lots +
+                '}';
+    }
+
+    public ParkingLot(int total) {
+        this.total = total;
+        this.lots = new ArrayList<>(Collections.nCopies(total, null));
+    }
+
+    public boolean isLotOccupied(int lotNumber) {
+        return lots.get(lotNumber) == null;
+    }
+
+    public int getEmptyLot(){
+        for (int i = 0; i < total; i++) {
+            if(lots.get(i) == null) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int getEmptyLotNumber(){
+        int emptyNum = 0;
+        for (int i = 0; i < total; i++) {
+            if(lots.get(i) == null) {
+                emptyNum++;
+            }
+        }
+        return emptyNum;
+    }
+
+    public void parkCar(Car car, ParkingTicket address) {
+        lots.set(address.getLotNumber(), car);
+        car.setLotAddress(address);
+    }
+
+    public Car pickCar(int lotNumber) {
+        Car car = lots.get(lotNumber);
+        if(car != null) {
+            lots.set(lotNumber, null);
+            car.setLotAddress(null);
+        }
+
+        return car;
     }
 }
