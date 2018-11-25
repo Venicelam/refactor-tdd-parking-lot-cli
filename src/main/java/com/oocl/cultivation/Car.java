2ea472a1272203da097012dc9b78875765c0c79a
lamve2 <venice.lam@oocl.com>
2018-11-25 15:43:34 +0800

    implement Car

--- src/main/java/com/oocl/cultivation/Car.java
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -1,4 +1,27 @@
 package com.oocl.cultivation;
 
 public class Car {
+    private String licenseNumber;
+    private ParkingTicket lotAddress = null;
+
+    public Car(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public ParkingTicket getLotAddress() {
+        return lotAddress;
+    }
+
+    public void setLotAddress(ParkingTicket lotAddress) {
+        this.lotAddress = lotAddress;
+    }
 }
+
