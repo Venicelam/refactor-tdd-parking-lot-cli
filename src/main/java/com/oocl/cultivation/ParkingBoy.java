72a11c576e71985bd91b3ce744dfbce47705a1fe
lamve2 <venice.lam@oocl.com>
2018-11-26 23:45:45 +0800

    remove unused method in ParkingBoy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -35,8 +35,6 @@ public class ParkingBoy {
        }
         return parkingLot.pickCar(lotAddress.getLotNumber());
     }
-    public String getLastErrorMessage() {
-        return Message;
-    }
+
 }
 
